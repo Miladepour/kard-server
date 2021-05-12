c790dbc0a809133ee7768024dbf483d99d23e573
Kwabena <67422830+KBA99@users.noreply.github.com>
2021-05-12 18:05:26 +0100

    WIP still working on store

--- controllers/store.js
+++ controllers/store.js
@@ -12,6 +12,11 @@ const storeController = {
     let userBoardID = userBoards[0]._id.toString()
     let userBoardTitle = userBoards[0].name.toString()
     let userBoardColumns = await ColumnModel.find({board: userBoardID})
+    // let userBoardColumnsFlat = userBoardColumns[0]
+    // let userBoardColumnIDs = []
+    // userBoardColumnsFlat.forEach(userBoardColumn => {
+    //   userBoardColumnIDs.push(userBoardColumn._id)
+    // })
     
     // const { _id } = userBoards[0]
 
@@ -23,7 +28,7 @@ const storeController = {
 
         },
         columnIds: [
-          userBoardColumns
+          userBoardColumns[0].forEach(column => {console.log(column)})
         ],
         boards: {
           [userBoardID]: {
