7db2936a042012e5bd82d842ca900adfe9f53b20
Dominic-Taylor-Dev <email.dom.taylor@gmail.com>
2021-05-14 11:15:46 +0100

    final stable release prior to demo

--- controllers/cards.js
+++ controllers/cards.js
@@ -35,10 +35,24 @@ const cardsController = {
   
   deleteOneByID: async(req, res) => { 
     let cardID = req.params.card_id
+    let card = await CardModel.find({_id: cardID})
     let cardColumn = await ColumnModel.find({cards: cardID})
+    cardColumn = cardColumn[0]
+    
+
+    let cardColumnAllCards = cardColumn.cards // array of all cards within column record, including one to be deleted
+    console.log(cardColumnAllCards)
+    console.log(cardID)
+
+    let cardColumnCardsMinusCardToDelete = cardColumnAllCards.filter(e => e.toString() !== cardID.toString())
+    
+    
+    cardColumn.cards = cardColumnCardsMinusCardToDelete
+    cardColumn.save()
+
     await CardModel.findByIdAndDelete(cardID).then((card) => {
       if (!card) {
-        return res.json({deletedCardID: cardID, finalUpdatedColumn: cardColumn[0]});
+        return res.json({deletedCardID: cardID, finalUpdatedColumn: cardColumn});
       }
       res.send(card);
     }).catch((error) => {
@@ -48,8 +62,11 @@ const cardsController = {
   
   changeNameByID: async(req, res) => {
     // {new: true} gets us back the updated version of the document rather than the info prior to making the change
+    console.log("Card ID:" + req.params.card_id)
+    console.log(req.body)
     CardModel.findByIdAndUpdate(req.params.card_id, req.body, {new: true}).then((card) => { 
-        if (!card) {
+      console.log(card)
+       if (!card) {
             return res.status(404).send();
         }
         res.send(card);
